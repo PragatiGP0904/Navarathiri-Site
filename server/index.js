bc2578a554ce458b1c2cf1e74a00befe8a926256
Pragati GP <gppragati@gmail.com>
2025-10-03 13:25:10 +0530

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('../'));
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
